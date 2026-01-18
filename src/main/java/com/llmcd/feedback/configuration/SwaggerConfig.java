29d2d54ff57bfb80beefdcae18149afacd4828da
Lucas Macedo <lucasmacedo1901@gmail.com>
2026-01-18 12:26:59 -0300

    Feat swagger

---
+++ src/main/java/com/llmcd/feedback/configuration/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.llmcd.feedback.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI().info(new Info()
+                .title("Feedback API")
+                .description("Documentation of Feedback API"));
+    }
+
+
+
+
+}
--- src/main/java/com/llmcd/feedback/controller/FeedbackController.java
+++ src/main/java/com/llmcd/feedback/controller/FeedbackController.java
@@ -3,6 +3,8 @@ package com.llmcd.feedback.controller;
 import com.llmcd.feedback.model.FeedBack;
 import com.llmcd.feedback.service.FeedBackService;
 import com.llmcd.feedback.shared.SendFeedBackDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "Feedback", description = "Management of feedbacks")
 public class FeedbackController {
 
     private final FeedBackService feedBackService;
@@ -22,6 +25,7 @@ public class FeedbackController {
     }
 
     @PostMapping("/sendFeedback")
+    @Operation(summary = "Register feedback", description = "rating valid range = 0 - 5")
     public ResponseEntity<?> feedbackInsert(@RequestBody SendFeedBackDTO data) throws BadRequestException {
         Integer id = feedBackService.saveFeedback(data.iduser(), data.rating(), data.comment(), data.postId());
         HashMap<String, String> response= new HashMap<>();
@@ -31,6 +35,7 @@ public class FeedbackController {
     }
 
     @GetMapping("/serchFeedback")
+    @Operation(summary = "Find feedbacks of a postage", description = "Use param postId")
     public Optional<List<FeedBack>> feedbackSearch(@RequestParam Integer postId){
         return feedBackService.findFeedbackPost(postId);
     }
